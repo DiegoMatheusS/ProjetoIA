7e35189400340d796431fa8ea25b8fb44655416f
Diego Matheus <152340543+DiegoMatheusS@users.noreply.github.com>
2026-09-17 16:36:42 -0300

    Registra rotas OAuth do Mercado Livre

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from .api import app
+from .auth.mercadolivre_router import router as mercadolivre_router
 from .shopee.router import router as shopee_router
 
 app.include_router(shopee_router)
+app.include_router(mercadolivre_router)
 
 __all__ = ["app"]
