ffe89dd0ce4b07848121c69b4e6e54518a566834
Diego Matheus <152340543+DiegoMatheusS@users.noreply.github.com>
2026-09-20 14:24:20 -0300

    Registra rotas comerciais do Mercado Livre

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from .api import app
-from .auth.mercadolivre_router import router as mercadolivre_router
+from .auth.mercadolivre_router import router as mercadolivre_oauth_router
+from .mercadolivre.router import router as mercadolivre_api_router
 from .shopee.router import router as shopee_router
 
 app.include_router(shopee_router)
-app.include_router(mercadolivre_router)
+app.include_router(mercadolivre_oauth_router)
+app.include_router(mercadolivre_api_router)
 
 __all__ = ["app"]
