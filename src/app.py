cf463e969f142e5fcfecf2189aebdf44ce679b86
Diego Matheus <152340543+DiegoMatheusS@users.noreply.github.com>
2026-09-22 10:06:14 -0300

    feat: expose identical product offer search

--- src/app.py
+++ src/app.py
@@ -2,9 +2,11 @@ from .api import app
 from .auth.mercadolivre_router import router as mercadolivre_oauth_router
 from .mercadolivre.router import router as mercadolivre_api_router
 from .shopee.router import router as shopee_router
+from .offers.identical_product_router import router as identical_product_offers_router
 
 app.include_router(shopee_router)
 app.include_router(mercadolivre_oauth_router)
 app.include_router(mercadolivre_api_router)
+app.include_router(identical_product_offers_router)
 
 __all__ = ["app"]
